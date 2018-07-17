6b49fb2b76ec38b054d20cd7e8e65d0d8706465e
Kajak2137 <kajos200@gmail.com>
2018-07-17 19:16:50 +0200

    Another typo..

--- most_lib.py
+++ most_lib.py
@@ -18,14 +18,14 @@ def czymostjestotwarty():
     time_now = int(datetime.now(tz).strftime("%H%M"))
     if date_now in normalne:
         if 000 <= time_now <= 1030 or 1100 <= time_now <= 1200 or 1300 <= time_now <= 1330 or 1430 <= time_now <= 1600 or 1730 <= time_now <= 2359:
-            return "Otwarty do %s" % closest(normalne_godziny_zamkniecia, time_now)
+            return "Otwarty do %s" % get_closest(normalne_godziny_zamkniecia, time_now)
         else:
-            return "Zamknięty do %s" % closest(normalne_godziny_otwarcia, time_now)
+            return "Zamknięty do %s" % get_closest(normalne_godziny_otwarcia, time_now)
     elif date_now in wakacyjne:
         if 000 <= time_now <= 800 or 830 <= time_now <= 1030 or 1130 <= time_now <= 1200 or 1300 <= time_now <= 1330 or 1430 <= time_now <= 1600 or 1730 <= time_now <= 1830 or 1900 <= time_now <= 2030 or 2120 <= time_now <= 2359:
-            return "Otwarty do %s" % closest(wakacyjne_godziny_zamkniecia, time_now)
+            return "Otwarty do %s" % get_closest(wakacyjne_godziny_zamkniecia, time_now)
         else:
-            return "Zamknięty do %s" % closest(wakacyjne_godziny_otwarcia, time_now)
+            return "Zamknięty do %s" % get_closest(wakacyjne_godziny_otwarcia, time_now)
     elif date_now in zamkniete:
         return "Most otwarty tylko od kwietnia do października"
     else:
