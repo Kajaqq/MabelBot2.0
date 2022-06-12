5ff0b07cda929f5f8ab62d36d1b236cd0d4afa6f
Kajaqq <kajos200@gmail.com>
2022-06-12 18:31:44 +0200

    PEP8 fixes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MabelBot2.0
+
 Messenger bot based on fbchat library and inspired by Mabelbot by d3suu
--- hotfix.sh
+++ hotfix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git clone https://github.com/Kajak2137/MabelBot2.0  ./tmp
+git clone https://github.com/Kajaqq/MabelBot2.0  ./tmp
 rm ./tmp/hotfix.sh
 mv ./tmp/* .
 rm -rf ./tmp
-python3.6 MabelBot.py
+python MabelBot.py
