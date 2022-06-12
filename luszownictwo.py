b7fc5eb1d084e1fccb8bec64ddea6dd823f6d719
Kajaqq <kajos200@gmail.com>
2022-06-12 18:30:54 +0200

    Remove obsolete files

--- luszownictwo.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def lurczak():
-    with open("miejski slownik kurczaka.txt", "r") as f:
-        random.seed()
-        lines = f.read().split("\n")
-        return lines[random.randint(0, (len(lines) - 1))]
